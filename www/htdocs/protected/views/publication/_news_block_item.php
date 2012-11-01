9f2b773fa4930f2e93d66097707f7dd621ad4b0b
jetli13 <atcmsframework@yandex.ru>
2012-11-01 21:45:59 +0400

    увеличил количество символов в сниппете новости

--- www/htdocs/protected/views/publication/_news_block_item.php
+++ www/htdocs/protected/views/publication/_news_block_item.php
@@ -5,7 +5,7 @@
 	<h2><? echo $data->ru_title ?></h2>
 	<p><a href="<?=$link?>">
 			<? 
-				$TEXT_LIMIT = 50;
+				$TEXT_LIMIT = 80;
 				print Publication::getPreview($data->ru_text, $TEXT_LIMIT); 
 			?>
 			</a>
